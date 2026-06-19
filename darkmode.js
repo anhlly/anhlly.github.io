315d71a3f67c8516f91578fb9f8209f9560835cf
anhlly <94507800+anhlly@users.noreply.github.com>
2026-06-19 15:01:32 -0400

    fix so the

--- darkmode.js
+++ darkmode.js
@@ -1,9 +1,9 @@
 let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementByID ('theme-switch')
+const themeSwitch = document.getElementById ('theme-switch')
 
 const enableDarkmode = () => {
     document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode','active')
+    localStorage.setItem('darkmode', 'active')
 }
 
 const disableDarkmode = () => {
@@ -15,5 +15,5 @@ if(darkmode === "active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode')
-    darkmode != "active" ? enableDarkmode() : disableDarkmode()
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
